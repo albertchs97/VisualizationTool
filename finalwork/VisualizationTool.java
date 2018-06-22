ac0a9cbe607f00cdb9ec14e87610beebf4b8b6d8
Albert Chong <albertchs97@outlook.com>
2018-06-22 20:34:23 +0800

    Update VisualizationTool.java
    增加了数据导入功能

--- finalwork/VisualizationTool.java
+++ finalwork/VisualizationTool.java
@@ -1,19 +1,34 @@
 package finalwork;
 
+import java.awt.Color;
 import java.awt.Container;
+import java.awt.FileDialog;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JToggleButton;
+
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 
 public class VisualizationTool extends JFrame{
@@ -34,7 +49,6 @@ public class VisualizationTool extends JFrame{
 	Container con;	// 容器
 	JPanel pan;	//面板
 	JTextArea TestArea = new JTextArea("什么也没有");
-	
 	JPanel Panel1 = new JPanel();
 	JPanel Panel2 = new JPanel();
 	JToggleButton MousePointer = new JToggleButton("MousePointer");
@@ -42,6 +56,55 @@ public class VisualizationTool extends JFrame{
 	JToggleButton VerticalZoom  = new JToggleButton("VerticalZoom");
 	JToggleButton Zoom = new JToggleButton("Zoom");
 	JButton Reset = new JButton("Reset");
+	
+	double[] t;
+	double[] data;
+	int datalength = 0;
+	FileDialog fd = new FileDialog(this);
+	String eFilePath = null;
+	String eFile = null;
+	
+	JFrame frame_HA = new JFrame("HA");
+	JFrame frame_PC = new JFrame("PC");
+	JFrame frame_FT = new JFrame("FT");
+	JFrame frame_SA = new JFrame("SA");
+	
+	//frame_HA setting
+	JPanel Panel_HA = new JPanel();
+	//frame_PC setting
+	JPanel Panel_PC = new JPanel();
+	//frame_FT setting
+	//JPanel_FT Panel_FT;
+	//frame_SA setting
+	JPanel Panel_SA = new JPanel();
+	
+	
+	
+	
+	/*
+	public void frame_HA_init(){
+		frame_HA.setTitle("谐波分析");
+		//设置窗体大小
+		frame_HA.setSize(300, 300);
+	}
+	public void frame_PC_init(){
+		frame_PC.setTitle("功率计算");
+		//设置窗体大小
+		frame_PC.setSize(300, 300);
+	}
+	public void frame_FT_init(){
+		frame_FT.setTitle("傅里叶变换");
+		//设置窗体大小
+		frame_FT.setSize(300, 300);
+		
+
+	}
+	public void frame_SA_init(){
+		frame_SA.setTitle("频谱分析");
+		//设置窗体大小
+		frame_SA.setSize(300, 300);
+	}
+	*/
 	
 	public VisualizationTool(){
 		//设置标题栏
@@ -80,9 +143,46 @@ public class VisualizationTool extends JFrame{
 		con.add(pan);	//在容器中添加面板
 		this.setVisible(true);
 		
+		
 		mFile_Import.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				TestArea.setText("导入数据");
+				//设置对话框的模式是打开LOAD模式
+				fd.setMode(FileDialog.LOAD);
+				//设置对话框标题
+				fd.setTitle("选择数据文件");
+				//设置只显示excel的文件
+				fd.setFile("*.xlsx");
+				//配置好对话框的所有参数（包括文件夹路径，文件类型，窗口标题等）之后，再让对话框显示
+				fd.setVisible(true);
+				eFilePath = fd.getDirectory();
+				eFile = fd.getFile();
+				
+				//读取数据文件
+				String FileName = eFilePath + eFile;
+				File file = new File(FileName);
+				//读取xlsx
+				try{
+					InputStream is = new FileInputStream(file);
+					XSSFWorkbook xssfWorkbook = new XSSFWorkbook(is);
+					XSSFSheet xssfSheet = xssfWorkbook.getSheetAt(0);
+					datalength = xssfSheet.getLastRowNum()+1;
+					t = new double[datalength];
+					data = new double[datalength];
+					for (int rowNum = 0; rowNum <= xssfSheet.getLastRowNum(); rowNum++) {
+						XSSFRow xssfRow = xssfSheet.getRow(rowNum);
+						if (xssfRow != null) {
+						        //读取第一列数据-时间
+						        t[rowNum] = xssfRow.getCell(0).getNumericCellValue();
+						        //读取第二列数据-电压/电流
+						        data[rowNum] = xssfRow.getCell(1).getNumericCellValue();
+						    }
+						}		
+				}catch(Exception e1){
+					e1.printStackTrace();
+				}
+				
+				TestArea.setText("文件名: " + FileName + 
+						" 数据长度: " + String.valueOf(datalength));
 			}
 		});
 		
@@ -114,7 +214,20 @@ public class VisualizationTool extends JFrame{
 		mTools_FT.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				TestArea.setText("FT");
+				frame_FT.setTitle("傅里叶变换");
+				frame_FT.setSize(300, 300);
+				JPanel_FT Panel_FT = new JPanel_FT(datalength,t,data);
+				
+				JPanel Panel_FT2 = new JPanel();//
+				JLabel test1 = new JLabel("test1");//
+				Panel_FT2.add(test1);
+				frame_FT.add(Panel_FT);
+				frame_FT.add(Panel_FT2);//
+				Panel_FT.repaint();
+				frame_FT.setVisible(true);
+				
 			}
+			
 		});
 		
 		mTools_SA.addActionListener(new ActionListener() {
@@ -129,5 +242,32 @@ public class VisualizationTool extends JFrame{
 		VisualizationTool VT1 = new VisualizationTool();
 		
 	}
-
+	
+	class JPanel_FT extends JPanel{
+		ArrayList lines;
+		Point2D.Double beginpoint;
+		Point2D.Double endpoint;
+		public JPanel_FT(int data_length,double[] t,double[] data){
+			lines = new ArrayList();
+			for(int i=0;i<data_length-1;i++){
+				beginpoint = new Point2D.Double(t[i],data[i]);
+				endpoint = new Point2D.Double(t[i+1],data[i+1]);
+				Line2D line = new Line2D.Double(beginpoint,endpoint);
+				lines.add(line);
+			}
+			System.out.println(lines.size());
+		} 
+		public void paint(Graphics g){  
+			//super.paint(g);
+			
+			Graphics2D g2 = (Graphics2D)g;      // draw all lines
+			g2.setColor(Color.red);
+			for (int i = 0; i < lines.size(); i++)
+		    	g2.draw((Line2D)lines.get(i));
+			} 
+	}
+	
+	
 }
+
+
